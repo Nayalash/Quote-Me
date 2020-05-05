53187a8905741122b0d5e1c26756147045dbbe5a
Nayalash <1nayalashm@gmail.com>
2020-05-05 01:41:11 -0400

    added support for bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
     <div class="container">
-
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from "./components/QuoteGrid";
+
     export default {
+        data: function () {
+            return {
+                quotes: [
+                    "An empty quote, to get you started!"
+                ],
+                maxQuotes: 10
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid
+        }
 
     }
 </script>
